40e9ad9846d51dad6c7fffbd22f411516db461c1
avinashkitchensat <developer@loyalhospitality.in>
2019-07-30 17:10:50 +0530

    fifth commit

--- src/git/demo/tort/Hello.java
+++ src/git/demo/tort/Hello.java
@@ -7,6 +7,8 @@ System.out.println("Hello");
 System.out.println("Hello Avinash");
 System.out.println("Hello Avinash123");
 System.out.println("Hello Avinash123456");
+System.out.println("Hello Avinash1234567");
+System.out.println("Hello Avinash12345678");
 	}
 
 }
